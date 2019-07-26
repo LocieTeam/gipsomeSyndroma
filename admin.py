9107cd44643317caab1e93b6a40b2aba6894ccca
Locie <locieteam2019@gmail.com>
2019-07-27 01:50:47 +0530

    Remove

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Servei,AdsRequest
-
-
-# Register your models here.
-
-admin.site.register(Servei)
-admin.site.register(AdsRequest)
--- apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GipsomeappConfig(AppConfig):
-    name = 'gipsomeApp'
